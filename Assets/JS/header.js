1da15aa499793e8c9e7105e1ca07bb682ebd8cd1
Kurumi <minhneahihi123@gmail.com>
2025-10-04 23:27:44 +0700

    chia code

--- Assets/JS/header.js
+++ Assets/JS/header.js
@@ -2,4 +2,15 @@ fetch("header.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
+    /*toggle menu icon */
+    const menuIcon = document.querySelector("#menu-icon");
+    const navBarMenuIconActive = document.querySelector(
+      ".nav-bar-menu-icon-active"
+    );
+
+    menuIcon.addEventListener("click", () => {
+      navBarMenuIconActive.classList.toggle("active");
+      menuIcon.classList.toggle("fa-bars");
+      menuIcon.classList.toggle("fa-x");
+    });
   });
