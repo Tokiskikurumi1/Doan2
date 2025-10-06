30f3cecfd861e34cc5dcae24581e6c3efd55e6f7
Kurumi <minhneahihi123@gmail.com>
2025-10-07 00:50:22 +0700

    test-last

--- Assets/JS/header.js
+++ Assets/JS/header.js
@@ -1,16 +1,14 @@
-fetch("./Assets/html/header.html")
+fetch("Assets/html/header.html")
   .then((response) => {
     if (!response.ok) throw new Error("Không tìm thấy header.html");
     return response.text();
   })
   .then((data) => {
     document.getElementById("main-header").innerHTML = data;
-    /*toggle menu icon */
     const menuIcon = document.querySelector("#menu-icon");
     const navBarMenuIconActive = document.querySelector(
       ".nav-bar-menu-icon-active"
     );
-
     menuIcon.addEventListener("click", () => {
       navBarMenuIconActive.classList.toggle("active");
       menuIcon.classList.toggle("fa-bars");
