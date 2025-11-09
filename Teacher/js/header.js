d22d1c895caa8b43204166d5728edef8495fedab
Kurumi <minhneahihi123@gmail.com>
2025-11-09 22:18:08 +0700

    reponsive tổng quan giảng viên

--- Teacher/js/header.js
+++ Teacher/js/header.js
@@ -1,24 +1,39 @@
-fetch("header.html")
-  .then((response) => {
-    if (!response.ok) throw new Error("Không tìm thấy header.html");
-    return response.text();
-  })
-  .then((data) => {
-    document.getElementById("header").innerHTML = data;
-    /*toggle menu icon */
-    const menuIcon = document.querySelector("#menu-icon");
-    const navBarMenuIconActive = document.querySelector(
-      ".nav-bar-menu-icon-active"
-    );
+const menuIcon = document.getElementById("menuIcon");
+const leftSlide = document.querySelector(".left-slide");
+const overlay = document.getElementById("overlay");
+// Load Sidebar
+fetch("components/sidebar.html")
+  .then((response) => response.text())
+  .then((html) => {
+    document.getElementById("sidebar-placeholder").innerHTML = html;
+    // highlightActiveNav();
+  });
 
+// Load Header
+fetch("components/header.html")
+  .then((response) => response.text())
+  .then((html) => {
+    document.getElementById("header-placeholder").innerHTML = html;
+    MenuToggle();
+  });
+
+// Menu toggle
+function MenuToggle() {
+  const menuIcon = document.getElementById("menuIcon");
+  const leftSlide = document.querySelector(".left-slide");
+  const overlay = document.getElementById("overlay");
+
+  if (menuIcon && leftSlide && overlay) {
     menuIcon.addEventListener("click", () => {
-      navBarMenuIconActive.classList.toggle("active");
-      menuIcon.classList.toggle("fa-bars");
-      menuIcon.classList.toggle("fa-x");
+      leftSlide.classList.toggle("active");
+      overlay.classList.toggle("active");
     });
-    updateLoginStatus();
-  })
-  .catch((error) => console.error("Lỗi fetch:", error));
+    overlay.addEventListener("click", () => {
+      leftSlide.classList.remove("active");
+      overlay.classList.remove("active");
+    });
+  }
+}
 
 function updateLoginStatus() {
   const loginTable = document.querySelector(".login");
